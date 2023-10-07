49dfb8f0afaec1c6e709ba329ba489f157184a66
Jean Pons <jeanpons183@gmail.com>
2023-10-07 13:18:37 -0600

    Finalización de archivo de prueba

--- Prueba.py
+++ Prueba.py
@@ -2,28 +2,37 @@ import RPi.GPIO as GPIO
 import time
 
 # Define the GPIO pins for PWM, direction, and enable
-PWM_PIN = 13        
-DIR_PIN = 19
-EN_PIN = 3
+PWMR_PIN = 13        
+PWML_PIN = 12        
+EN_PIN = 2
 
 # Initialize GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PWM_PIN, GPIO.OUT)
-GPIO.setup(DIR_PIN, GPIO.OUT)
+GPIO.setup(PWMR_PIN, GPIO.OUT)
+GPIO.setup(PWML_PIN, GPIO.OUT)
 GPIO.setup(EN_PIN, GPIO.OUT)
 
 # Set up PWM
-pwm = GPIO.PWM(PWM_PIN, 1000)  # 1000 Hz PWM frequency
-pwm.start(0)  # Start with 0% duty cycle
+pwmr = GPIO.PWM(PWMR_PIN, 1000)  # 1000 Hz PWM frequency
+pwml = GPIO.PWM(PWML_PIN, 1000)  # 1000 Hz PWM frequency
+
+pwmr.start(0)  # Start with 0% duty cycle
+pwml.start(0)  # Start with 0% duty cycle
 
 # Motor control functions
 def set_motor_speed(speed):
     print("set_motor_speed funci")
     if speed >= 0:
-        GPIO.output(DIR_PIN, GPIO.HIGH)  # Set direction forward
+        pwmr.ChangeDutyCycle(abs(speed))  # Set motor speed
+        pwml.ChangeDutyCycle(abs(0))  # Set motor speed      
+        # Set direction forward, don't do that, maybe cause PIN problem?
+        #, no connected correctly to motor drive? :/
+        
     else:
-        GPIO.output(DIR_PIN, GPIO.LOW)  # Set direction backward
-    pwm.ChangeDutyCycle(abs(speed))  # Set motor speed
+        pwmr.ChangeDutyCycle(abs(0))  # Set motor speed
+        pwml.ChangeDutyCycle(abs(speed))  # Set motor speed     
+        
+
 
 def enable_motor():
     print("enable_motor funci")
@@ -36,12 +45,12 @@ def disable_motor():
 # Main code
 try:
     print("hello")
-    enable_motor()
-    set_motor_speed(-50)  # Set motor speed (0-100, positive for forward, negative for backward)
-    time.sleep(5)  # Run the motor for 5 seconds
-    set_motor_speed(50)  # Set motor speed (0-100, positive for forward, negative for backward)
-    time.sleep(5)  # Run the motor for 5 seconds
-
+    while True:
+        enable_motor()
+        set_motor_speed(-50)  # Set motor speed (0-100, positive for forward, negative for backward)
+        time.sleep(5)  # Run the motor for 5 seconds
+        set_motor_speed(50)  # Set motor speed (0-100, positive for forward, negative for backward)
+        time.sleep(5)  # Run the motor for 5 seconds
 
 except KeyboardInterrupt:
     pass
@@ -50,3 +59,4 @@ finally:
     print("bye")
     disable_motor()
     GPIO.cleanup()  # Cleanup GPIO on program exit
+    time.sleep(3)
