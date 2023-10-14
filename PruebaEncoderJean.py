0d427cab071589e67dd2407532d3f5977853004a
Jean Pons <jeanpons183@gmail.com>
2023-10-14 14:32:30 -0600

    Completando PruebaMotorEncoder en PrueaEncoderJean

--- PruebaEncoderJean.py
+++ PruebaEncoderJean.py
@@ -26,32 +26,36 @@ def actualizar_posicion(channel):
     print("Posición:", posicion)
 
 try:
-    # Configuración de GPIO
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(RPWM, GPIO.OUT)
-    GPIO.setup(LPWM, GPIO.OUT)
-    GPIO.setup(EN_PWM, GPIO.OUT)
-    GPIO.setup(ENCODER_A, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(ENCODER_B, GPIO.IN)
-    GPIO.add_event_detect(ENCODER_A, GPIO.RISING, callback=actualizar_posicion,bouncetime=50)  # Al meterlo a un ciclo vale cheto
-    
-    # Crear objetos PWM
-    rpwm = GPIO.PWM(RPWM, 1000)
-    lpwm = GPIO.PWM(LPWM, 1000)
-    en_pwm = GPIO.PWM(EN_PWM, 1000)
+	# Configuración de GPIO
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(RPWM, GPIO.OUT)
+	GPIO.setup(LPWM, GPIO.OUT)
+	GPIO.setup(EN_PWM, GPIO.OUT)
+	GPIO.setup(ENCODER_A, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	GPIO.setup(ENCODER_B, GPIO.IN)
+	GPIO.add_event_detect(ENCODER_A, GPIO.RISING, callback=actualizar_posicion,bouncetime=50)  # Al meterlo a un ciclo vale cheto
+
+	# Crear objetos PWM
+	rpwm = GPIO.PWM(RPWM, 1000)
+	lpwm = GPIO.PWM(LPWM, 1000)
+	en_pwm = GPIO.PWM(EN_PWM, 1000)
 
     # Iniciar PWM
-    rpwm.start(0)
-    lpwm.start(0)
-    en_pwm.start(100)
+	rpwm.start(0)
+	lpwm.start(5)
+	en_pwm.start(100)
 
-    while True:
-        # Cambiar la velocidad del motor 
-        rpwm.ChangeDutyCycle(70)  # (ajusta según tus requerimientos)
-
-        # Imprimir la posición actual del encoder
-        print("Posición:", posicion)
-        sleep(0.1)
+	while True:
+		if(posicion > 65):
+			rpwm.ChangeDutyCycle(0)  # (ajusta según tus requerimientos)
+			lpwm.ChangeDutyCycle(5)  # (ajusta según tus requerimientos)
+		elif(posicion <0):
+			# Cambiar la velocidad del motor 
+			rpwm.ChangeDutyCycle(5)  # (ajusta según tus requerimientos)
+			lpwm.ChangeDutyCycle(0)  # (ajusta según tus requerimientos)
+		# Imprimir la posición actual del encoder
+		print("Posición:", posicion)
+		sleep(0.1)
 
 except KeyboardInterrupt:
     pass
@@ -63,3 +67,5 @@ finally:
     en_pwm.stop()
     GPIO.cleanup()
     print("Programa terminado.")
+    sleep(10)
+
