73f3072c2ad5261f9b734963968142148bc28729
Jean Bryan Pons Cisneros <130619978+JeanPons183@users.noreply.github.com>
2023-10-14 13:25:23 -0600

    Update PruebaEncoderJean.py

--- PruebaEncoderJean.py
+++ PruebaEncoderJean.py
@@ -1,33 +1,53 @@
 import RPi.GPIO as GPIO
-import sys
-import signal
 from time import sleep
 
 # Pin del motor:
-	#Rojo motor+
-	#Negro motor-
-	#Verde gnd
-	#Azul vcc encoder 3.3V
-	#Amarillo salida A, encoder adelante activa primero este
-	# Blanco salida b, encoder reversa activa primero este
-	
-print("Prueba unicamente del encoder")
+#   Rojo motor+
+#   Negro motor-
+#   Verde GND
+#   Azul VCC encoder 3.3V
+#   Amarillo salida A, encoder adelante (activa primero este)
+#   Blanco salida B, encoder reversa (activa primero este)
+
+print("Prueba únicamente del encoder")
 
 encoderA = 11
 encoderB = 13
 posicion = 0
 
-print("Posicion inicial:", posicion)
+print("Posición inicial:", posicion)
 
-#Configuración Raspberry pines
+# Configuración Raspberry pines
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(encoderA,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(encoderB,GPIO.IN)
+GPIO.setup(encoderA, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(encoderB, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+def actualizar_posicion(channel):
+    global posicion
+    lecturaSignalA = GPIO.input(encoderA)
+    lecturaSignalB = GPIO.input(encoderB)
 
-lecturaSignalA = GPIO.input(encoderA)
-lecturaSignalB = GPIO.input(encoderB)
+    # Determinar la dirección y sentido del giro del encoder
+    if lecturaSignalA == lecturaSignalB:
+        posicion += 1
+    else:
+        posicion -= 1
 
-while(True):
-	print(a)
-	print(b)
+# Configurar la detección de eventos en ambos pines del encoder
+GPIO.add_event_detect(encoderA, GPIO.BOTH, callback=actualizar_posicion)
+GPIO.add_event_detect(encoderB, GPIO.BOTH, callback=actualizar_posicion)
+
+try:
+    while True:
+        # Imprimir la posición actual del encoder
+        print("Posición:", posicion)
+        sleep(0.1)  # Esperar un corto período para evitar un bucle continuo
+
+except KeyboardInterrupt:
+    pass
+
+# Limpieza de recursos GPIO al finalizar
+finally:
+	GPIO.cleanup()
+	print("Adios :D")
+	sleep(3)
