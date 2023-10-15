9ebf53ecabdb4234a5b4753c87ded1e9a772ae1f
Jean Pons <jeanpons183@gmail.com>
2023-10-14 19:19:28 -0600

    Añadiendo comentarios y optimizando PDMotorEncoder

--- PDMotorEncoder.py
+++ PDMotorEncoder.py
@@ -11,14 +11,14 @@ kd = 0.006
 
 tSimulacion = 25
 
-#Inicializar listas
-tiempo=[]
+#Inicializar listas, (tiempo,posicion,AccionControl,posicionDeseada y error)
+tiempo=[]		
 pos = []
 control = []
-pdPlot=[]
+pdPlot=[]	
 errorPlot = []
 
-# Definición de pines
+# Definición de pines del tipo BOARD
 RPWM = 32
 LPWM = 33
 EN_PWM = 35
@@ -30,64 +30,60 @@ posicion = 0
 
 def actualizar_posicion(channel):
     global posicion
-    if GPIO.input(ENCODER_A) == GPIO.HIGH:
-        if GPIO.input(ENCODER_B) == GPIO.LOW:
+    if GPIO.input(ENCODER_A) == GPIO.HIGH:	#Cuando detecta el flanco A 
+        if GPIO.input(ENCODER_B) == GPIO.LOW:	#Si el flanco B esta abajo, se movió hacia adelante
             posicion += 1
-        else:
-            posicion -= 1
-    else:
-        if GPIO.input(ENCODER_B) == GPIO.HIGH:
-            posicion += 1
-        else:
+        else:					#Sino pos se movió para atras
             posicion -= 1
     print("Posición:", posicion)
     
 def setMotor(direccion,u):
 	if(direccion==1):
-		rpwm.ChangeDutyCycle(abs(u))  # (ajusta según tus requerimientos)
-		#rpwm.ChangeDutyCycle(10)  # Prueba de la funcion
-		lpwm.ChangeDutyCycle(0)  # (ajusta según tus requerimientos)
+		rpwm.ChangeDutyCycle(abs(u))  # ajusta según el control
+		lpwm.ChangeDutyCycle(0)  # Si se mueve para adelante, entonces el lpwm es 0
 	else:
-		lpwm.ChangeDutyCycle(abs(u))  # (ajusta según tus requerimientos)
-		rpwm.ChangeDutyCycle(0)  # (ajusta según tus requerimientos)
-		#lpwm.ChangeDutyCycle(10)  # pruea de la fucnion
+		lpwm.ChangeDutyCycle(abs(u))  # ajusta según el control
+		rpwm.ChangeDutyCycle(0)  # Si se mueve para atras, entonces el rpwm es 0
 
 try:
-	# Configuración de GPIO
+	# Configuración de Raspberry Pi GPIO
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(RPWM, GPIO.OUT)
 	GPIO.setup(LPWM, GPIO.OUT)
 	GPIO.setup(EN_PWM, GPIO.OUT)
-	GPIO.setup(ENCODER_A, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	GPIO.setup(ENCODER_A, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)	# Configurado como PullDown
 	GPIO.setup(ENCODER_B, GPIO.IN)
-	GPIO.add_event_detect(ENCODER_A, GPIO.RISING, callback=actualizar_posicion,bouncetime=50)  # Al meterlo a un ciclo vale cheto
+	GPIO.add_event_detect(ENCODER_A, GPIO.RISING, callback=actualizar_posicion,bouncetime=50)  # Configuracion que detecta flancos 
 
 	# Crear objetos PWM
 	rpwm = GPIO.PWM(RPWM, 1000)
 	lpwm = GPIO.PWM(LPWM, 1000)
 	en_pwm = GPIO.PWM(EN_PWM, 1000)
 
-	# Iniciar PWM
+	# Inicializar PWM
 	rpwm.start(0)
-	lpwm.start(5)
+	lpwm.start(0)
 	en_pwm.start(100)
 
+	# Inicializar parametros control	
 	errorAnt = 0
 	tiempoAnterior = 0
 	t = 0
-	error = pd-posicion
-	i=1
-	# ~ while abs(error)>0.001:
-	while t<tSimulacion:
+	i=1 	# Para agregar el primer elemento, que la diferencia de tiempo es mucha
+	direccion = 0
+	
+	# ~ while abs(error)>0.001:			#Esto es para parar el ciclo por error
+	while t<tSimulacion:				#Esto es para parar el ciclo por tiempo
+		
 		#Calculo del tiempo
 		tiempoActual=time()
-		deltaTiempo = tiempoActual-tiempoAnterior
-		tiempoAnterior = tiempoActual
+		deltaTiempo = tiempoActual-tiempoAnterior	#Diferencia de tiempo
+		tiempoAnterior = tiempoActual			# El tiempo anterior se convierte en el actual
 		
 		#Calculo parte derivativa
-		error = pd-posicion
-		dError = (error-errorAnt)/deltaTiempo
-		errorAnt = error
+		error = pd-posicion				# Calculo del error
+		dError = (error-errorAnt)/deltaTiempo		# Derivada del tiempo
+		errorAnt = error				
 		
 		# Ley de control
 		u = kp*error+kd*dError
@@ -96,35 +92,38 @@ try:
 		if(abs(u)>80):
 			u=80
 		
-		direccion = 1
+		# Cambio de dirección dependiendo la ley de control
 		if(u<0):
 			direccion=-1
+		else:
+			direccion=1
 			
-		setMotor(direccion,abs(u))
-		
-		pos.append(posicion)
+		# Llamada al control de motores
+		setMotor(direccion,abs(u))	
 		
 		if(i==1):
+			i+=1
 			t += 0
 		else:
 			t += deltaTiempo
 				
 		print("Tiempo = ",t)
-		tiempo.append(t)
+		tiempo.append(t)		#Añade el tiempo
 			
 		# Imprimir la posición actual del encoder
 		print("Posición:", posicion)
+		pos.append(posicion)		# Añadir a la lista la posicion actual
+		pdPlot.append(pd)		# Añade a la lista la posición deseada
 		
 		# Imprimir error
 		print("Error: ",error)
-		errorPlot.append(error)
+		errorPlot.append(error)		# Añade el error
 		
 		#Imprimir control
 		print("El control es de: ",u)
-		control.append(u)
-		pdPlot.append(pd)
-		i+=1
-		sleep(0.1)
+		control.append(u)		# Añade la acción de control
+		
+		sleep(0.1)	# Para evitar problemas de lectura de datos
 
 except KeyboardInterrupt:
     pass
