dd5292c5f38b44901071c093eded1bfd2c3dc8f3
Jean Bryan Pons Cisneros <jeanpons@hotmail.es>
2023-10-21 10:44:27 -0600

    Configuracion Servidor Lap

--- Pruebas/Prueba Blutu/PruebaConeccionLap.py
+++ Pruebas/Prueba Blutu/PruebaConeccionLap.py
@@ -1,15 +1,22 @@
 import socket
 
 # Configura el cliente
-server_host = 'Dirección IP de la Raspberry Pi'  # La dirección IP de la Raspberry Pi en la red local
+server_host = '192.168.0.44'  # La dirección IP de la Raspberry Pi en la red local
 server_port = 12345  # Puerto de escucha (debe coincidir con el puerto del servidor)
 
 # Crea el socket del cliente
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((server_host, server_port))
 
-# Envía datos al servidor
-data_to_send = "Hola, Raspberry Pi"
-client_socket.send(data_to_send.encode())
+try:
+    while True:
+        # Envía datos al servidor
+        data_to_send = "Hola, Raspberry Pi"
+        pd = input("Ingrese posición deseada: ")
+        client_socket.send(data_to_send.encode())
+        client_socket.send(pd.encode())
+except KeyboardInterrupt:
+    pass
 
-client_socket.close()
+finally:
+    client_socket.close()
